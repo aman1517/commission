c09844693ddadcce300a7880d064e8f61dd59b8e
Akshay Yadav <akshay.yadav@bookmyinsurance.com>
2025-08-26 13:17:14 +0530

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,7 @@ const connectDB = async () => {
     
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-   
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    
   } catch (err) {
     console.error(`Error: ${err.message}`);
     process.exit(1);
